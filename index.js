e03fd9e6b32c68320d4f91bafb91bda7af09faca
Cephas Mwai <cephasmwai561@gmail.com>
2025-03-30 10:58:33 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution here
+function superbowlWin(record) {
+    
+    const winningYear = record.find(game => game.result === "W");
+    
+    
+    return winningYear ? winningYear.year : undefined;
+}
+
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.startsWith(letters));
+}
+
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+}
+
+
+module.exports = { superbowlWin, findMatching, fuzzyMatch, matchName };
